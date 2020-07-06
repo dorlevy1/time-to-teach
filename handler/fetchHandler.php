a40c257684266ab4dfbb780eec86ef278a47c778
Reuven Benitah <=>
2020-07-06 16:39:15 +0300

    reu

--- handler/fetchHandler.php
+++ handler/fetchHandler.php
@@ -3,7 +3,7 @@
 require_once '../Model/Calendar.php';
 require_once '../Model/User.php';
 $calendar = new Calendar();
-$user = new User();
+$user = new User();//
 if($user->fecthPersonalDetails()[0]['user_type']=='1'){
 
     return $calendar->fetchTeacherClasses($_SESSION['user_id']);
