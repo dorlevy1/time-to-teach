2a865140bf2367896fa26c0297860b0a213e2863
dor <ddl105095@gmail.com>
2020-07-06 15:21:05 +0300

    sql

--- handler/fetchHandler.php
+++ handler/fetchHandler.php
@@ -1,7 +1,13 @@
 <?php
 
 require_once '../Model/Calendar.php';
+require_once '../Model/User.php';
 $calendar = new Calendar();
+$user = new User();
+if($user->fecthPersonalDetails()[0]['user_type']=='1'){
 
+    return $calendar->fetchTeacherClasses($_SESSION['user_id']);
+}else{
+    return $calendar->fetchStudentClasses($_SESSION['user_id']);
 
-return $calendar->fetchAllClasses();
+}
